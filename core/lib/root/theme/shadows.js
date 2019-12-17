04dd1dc5a362588bcec997d7b10300353e0bbf87
Evgeniy Bogdanov <bogdanov.jn@gmail.com>
2019-12-17 15:20:14 +0200

    Fix for button box-shadow

--- core/lib/root/theme/shadows.js
+++ core/lib/root/theme/shadows.js
@@ -1,10 +1,11 @@
 import black from './colors/black';
+import primary from './colors/primary';
 const shadows = {
   xs: '0px 2px 4px 0px ' + black.lighten4,
   sm: '0px 4px 8px 0px ' + black.lighten4,
   md: '0px 8px 16px 0px ' + black.lighten4,
   lg: '0px 16px 24px 0px ' + black.lighten4,
   xl: '0px 24px 32px 0px ' + black.lighten4,
-  button: '0px 0px 0px 4px rgb(0,174,178, 0.16)'
+  button: '0px 0px 0px 4px ' + primary.lighten4
 };
 export default shadows;
